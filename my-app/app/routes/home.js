05f950f5ede64fc5591545bf5e47080ac292ffdd
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-07-26 11:21:31 -0500

    passes data to component

--- my-app/app/routes/home.js
+++ my-app/app/routes/home.js
@@ -1,3 +1,24 @@
 import Route from '@ember/routing/route';
 
-export default class HomeRoute extends Route {}
+export default class HomeRoute extends Route {
+  async model() {
+    try {
+      const response = await fetch(
+        'https://next-system-ivory.vercel.app/api/instruments',
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      return {
+        brand: data[0].brand,
+      };
+    } catch (error) {
+      console.error('Failed to fetch instruments:', error);
+      return [];
+    }
+  }
+}
