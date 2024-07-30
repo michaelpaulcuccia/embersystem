0b021dd93c78754e50b55949b539699e7d105ce2
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-07-30 15:48:42 -0500

    menu is show&hide

--- my-app/app/components/on-click-button.js
+++ my-app/app/components/on-click-button.js
@@ -1,9 +1,12 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class OnClickButton extends Component {
+  @tracked isMenuVisible = false;
   @action
   handleClick() {
     console.log('hello');
+    this.isMenuVisible = !this.isMenuVisible;
   }
 }
