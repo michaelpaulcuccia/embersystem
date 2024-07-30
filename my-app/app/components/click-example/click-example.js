48f82b74caabe5c364aec674166cace6cb8b6888
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-07-30 15:33:38 -0500

    removes unused code

--- my-app/app/components/click-example/click-example.js
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-import { action } from '@ember/object';
-
-export default class ClickExampleComponent extends Component {
-  @action
-  handleClick() {
-    console.log('Button was clicked!');
-  }
-}
